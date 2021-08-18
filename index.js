40b984a038548b07b2416dc1333921f30c197268
max <max@local.com>
2021-08-18 11:11:09 +0800

    remove catch

--- index.js
+++ index.js
@@ -11,22 +11,18 @@ module.exports = function(path){
     const path = ctx.request.path;
     const query = ctx.request.query;
     const body = ctx.request.query;
-    try{
-      let response
-      if (ctx.request.method == 'GET'){
-        response = database.get(path, query);
-      }else if (ctx.request.method == 'POST'){
-        response = database.get(path, body);
-      }
-      
-      if (!response) {
-        await next()
-      } else {
-        ctx.response.body = response
-      }
-    }catch(e){
-      console.log("error:", e)
-      await next();
+
+    let response
+    if (ctx.request.method === 'GET'){
+      response = database.get(path, query);
+    }else if (ctx.request.method === 'POST'){
+      response = database.get(path, body);
+    }
+
+    if (!response) {
+      await next()
+    } else {
+      ctx.response.body = response
     }
   };
 }
